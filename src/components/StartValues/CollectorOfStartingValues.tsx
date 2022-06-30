a4ee9e63c5fa40d47e4a0d8dcc76cfb75de35953
Elena Samoilenko <nuclear.cover@gmail.com>
2022-06-30 17:18:31 +0300

    create css

--- src/components/StartValues/CollectorOfStartingValues.tsx
+++ src/components/StartValues/CollectorOfStartingValues.tsx
@@ -1,45 +1,57 @@
-import React, {useState} from 'react';
-import s from'./StartValues.module.css'
+import React, {ChangeEvent, useState} from 'react';
+import s from './CollectorOfStartingValues.module.css'
 import {Button} from '../Button/Button';
 
-export type CollectorOfstartingValuesType={
-  addStartValues:(start:string, max:string)=> void
-  error:boolean
+export type CollectorOfstartingValuesType = {
+  addStartValues: (start: string, max: string) => void
+  error: boolean
+  setStart: (start: string) => void
+  setMax: (max: string) => void
+  max: string
+  start: string
+  isReadyForCountSet: (ready: boolean) => void
 }
 
 
-export const CollectorOfStartingValues:React.FC<CollectorOfstartingValuesType> = ({addStartValues, error}) => {
-  const [max, setMax] = useState<string>('0')
-  const [start, setStart] = useState<string>('0')
-  // const [isDisabled, setIsDisabled] = useState<boolean>(false)
+export const CollectorOfStartingValues: React.FC<CollectorOfstartingValuesType> = ({addStartValues,error,setStart,setMax,max,start,isReadyForCountSet }) => {
 
-  // let error = +max<=0  || +start<=0 || (+max-+start) <=0
+  const setStartOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    isReadyForCountSet(false)
+    setStart(e.currentTarget.value)
+  }
+  const setMaxOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    isReadyForCountSet(false)
+    setMax(e.currentTarget.value)
+  }
 
   return (
 
-      <div className={s.wrapper}>
-        <div className={s.values}>
-          <div className={s.values__item}>
-            <span>Start value</span>
-            <input
-              value={start}
-              onChange={(e) => setStart(e.currentTarget.value)}
-              type="number"/>
-          </div>
-          <div>
-            <span>Max value</span>
-            <input
-              value={max}
-              onChange={(e) => setMax(e.currentTarget.value)}
-              type="number"/>
-          </div>
+    <div className={s.wrapper}>
+      <div className={s.values}>
+        <div className={s.values__item}>
+          <span className={s.value__name}>Start value:</span>
+          <input
+            value={start}
+            onChange={setStartOnChange}
+            type="number"
+            className={s.input}
+          />
         </div>
-        <Button
-          title={'SET'}
-          className={'active'}
-          onClick={()=>addStartValues(start,max)}
-          disabled={error}
-        />
+        <div className={s.values__item}>
+          <span className={s.value__name}>Max value:</span>
+          <input
+            value={max}
+            onChange={setMaxOnChange}
+            type="number"
+            className={s.input}
+          />
+        </div>
+      </div>
+      <Button
+        title={'SET'}
+        onClick={() => addStartValues(start, max)}
+        disabled={error}
+      />
     </div>
   );
 };
